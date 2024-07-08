2cf9b1b1f6f06473ac0319e5d9941e23c5075efa
Mert Sığırcı <mert_barki@hotmail.com>
2024-07-08 18:10:42 +0300

    It.IsAny<>() & Callback<>() implemented.

---
+++ XUnitDemo.Test/XUnitDemo.App/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XUnitDemo.App
+{
+    public class Calculator : ICalculatorService
+    {
+        private ICalculatorService _calculatorService;
+
+        public Calculator(ICalculatorService calculatorService)
+        {
+            _calculatorService = calculatorService;
+        }
+        public int Add(int x, int y)
+        {
+            return _calculatorService.Add(x, y);
+        }
+
+        public int Divide(int numberToBeDivided, int dividerNumber)
+        {
+            return _calculatorService.Divide(numberToBeDivided, dividerNumber);
+        }
+
+        public int Multiple(int x, int y)
+        {
+            return _calculatorService.Multiple(x, y);
+        }
+    }
+}
---
+++ XUnitDemo.Test/XUnitDemo.App/ICalculatorService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XUnitDemo.App
+{
+    public interface ICalculatorService
+    {
+        public int Add(int x, int y);
+
+        public int Multiple(int x, int y);
+
+        public int Divide(int numberToBeDivided, int dividerNumber);
+    }
+}
